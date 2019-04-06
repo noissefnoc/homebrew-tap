ad4760859ced253af1696e36bd338ac8213bbe5d
noissefnoc <noissefnoc@gmail.com>
2019-04-07 00:28:53 +0900

    pixela v0.0.4 release.

--- Formula/pixela.rb
+++ Formula/pixela.rb
@@ -2,7 +2,7 @@ class Pixela < Formula
   version '0.0.4'
   homepage 'https://github.com/noissefnoc/pixela-client-go'
   url "https://github.com/noissefnoc/pixela-client-go/releases/download/v0.0.4/pixela_v0.0.4_darwin_amd64.zip"
-  sha256 '9cc4f35c8c66fa298f361d3c21ef462657cd0813d79c593f3e19e7bd4ea6f701'
+  sha256 'b0091b969cfcc52c811b2c96445d0290e1678594b96376c4c95cf80fd47cfa75'
   head 'https://github.com/noissefnoc/pixela-client-go.git'
 
   def install
