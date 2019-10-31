6e48cdc75257391e4055cdec22c38312b6373091
noissefnoc <noissefnoc@gmail.com>
2019-10-31 22:43:38 +0900

    Update pptgrep to 0.0.2

--- Formula/pptxgrep.rb
+++ Formula/pptxgrep.rb
@@ -1,8 +1,8 @@
 class Pptxgrep < Formula
-  version '0.0.1'
+  version '0.0.2'
   homepage 'https://github.com/noissefnoc/pptxgrep'
-  url "https://github.com/noissefnoc/pptxgrep/releases/download/v0.0.1/pptxgrep_darwin_amd64.zip"
-  sha256 'b2c4c08a85ff4764c2f1bbc6c1aa3bdf7fb25cbb0250eba44d66d0f1ccb4b422'
+  url "https://github.com/noissefnoc/pptxgrep/releases/download/v0.0.2/pptxgrep_darwin_amd64.zip"
+  sha256 '09ba878c89a9085153de3b9ec971120200900910d83d31f4b9a66bbed5c6561e'
   head 'https://github.com/noissefnoc/pptxgrep.git'
 
   head do
